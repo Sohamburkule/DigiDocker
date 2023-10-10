eb75e377159b2e2dc5d84a7bf2d3a653a8c13fb3
Sohamburkule <sohamburkule01@gmail.com>
2023-10-10 17:04:24 +0530

    first commit

---
+++ DigiDocker/src/main/java/com/DigiDocker/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.DigiDocker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController 
+{
+	@RequestMapping("/home")
+	public String home()
+	{
+		return "home";
+	}
+	
+	@RequestMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+	
+	@RequestMapping("/register")
+	public String register()
+	{
+		return "register";
+	}
+}
